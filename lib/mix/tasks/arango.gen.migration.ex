b5f3e901b034f6c17fe2ccb87a7f422ee6fb30d9
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2019-12-03 19:35:02 +0400

    Feature/Migrations (#1)
    * feat(Migrations): add arango.gen.migration task
    
    * chore(Migrations): change error msg from ecto to arango, update README
    
    * feat(Migrations): add arango.migrate mix task
    
    * chore(Docs): update migration docs
    
    * chore(Migrations): add shell feedback to the migrator
    
    * feat(Migrations): enable rollbacks, add more configurability to the mix tasks
    
    * chore(Docs) update TODO and Available Functions sections
    
    * fix(Migrations): return :ok from migration functions

---
+++ lib/mix/tasks/arango.gen.migration.ex
@@ -0,0 +1,74 @@
+defmodule Mix.Tasks.Arango.Gen.Migration do
+  use Mix.Task
+
+  import Mix.Generator
+  import Macro, only: [camelize: 1, underscore: 1]
+
+  @shortdoc "Generates a new migration for the ArangoPhx repo"
+
+  @switches [
+    no_compile: :boolean,
+    no_deps_check: :boolean
+  ]
+
+  @impl true
+  def run(args) do
+    case OptionParser.parse!(args, strict: @switches) do
+      {_, [name]} ->
+        path = Path.join(priv_repo_path(), "migrations")
+        base_name = "#{underscore(name)}.exs"
+        file = Path.join(path, "#{timestamp()}_#{base_name}")
+
+        unless File.dir?(path), do: create_directory(path)
+
+        fuzzy_path = Path.join(path, "*_#{base_name}")
+
+        if Path.wildcard(fuzzy_path) != [] do
+          Mix.raise(
+            "migration can't be created, there is already a migration file with name #{name}."
+          )
+        end
+
+        assigns = [
+          mod: Module.concat([ArangoPhx.Repo, Migrations, camelize(name)])
+        ]
+
+        create_file(file, migration_template(assigns))
+
+        file
+
+      {_, _} ->
+        Mix.raise(
+          "expected arango.gen.migration to receive the migration file name, " <>
+            "got: #{inspect(Enum.join(args, " "))}"
+        )
+    end
+  end
+
+  defp priv_repo_path do
+    app = Keyword.fetch!(ArangoPhx.Repo.config(), :otp_app)
+    Path.join(Mix.Project.deps_paths()[app] || File.cwd!(), "priv/repo")
+  end
+
+  defp timestamp do
+    {{y, m, d}, {hh, mm, ss}} = :calendar.universal_time()
+    "#{y}#{pad(m)}#{pad(d)}#{pad(hh)}#{pad(mm)}#{pad(ss)}"
+  end
+
+  defp pad(i) when i < 10, do: <<?0, ?0 + i>>
+  defp pad(i), do: to_string(i)
+
+  embed_template(:migration, """
+  defmodule <%= inspect @mod %> do
+    alias ArangoPhx.Repo
+
+    def up do
+      :ok
+    end
+
+    def down do
+      :ok
+    end
+  end
+  """)
+end
---
+++ lib/mix/tasks/arango.migrate.ex
@@ -0,0 +1,133 @@
+defmodule Mix.Tasks.Arango.Migrate do
+  use Mix.Task
+
+  @shortdoc "Runs Migration/Rollback Functions From Your Migration Modules"
+
+  @aliases [
+    d: :dir
+  ]
+
+  @switches [
+    dir: :string
+  ]
+
+  @impl true
+  def run(args) do
+    case OptionParser.parse!(args, aliases: @aliases, strict: @switches) do
+      {[], []} ->
+        up()
+
+      {[dir: "up"], _} ->
+        up()
+
+      {_, ["up"]} ->
+        up()
+
+      {[dir: "down"], _} ->
+        down()
+
+      {_, ["down"]} ->
+        down()
+
+      {_, ["rollback"]} ->
+        down()
+
+      {_, _} ->
+        Mix.raise("Unknown arguments, #{inspect(Enum.join(args, " "))}")
+    end
+  end
+
+  defp up do
+    pending_migrations()
+    |> Enum.each(fn file_path ->
+      case apply(migration_module(file_path), :up, []) do
+        :ok ->
+          File.cwd!()
+          |> Path.join("lib/mix/tasks/.migrated_versions")
+          |> File.write!("#{timestamp(file_path)}\n", [:append])
+
+          Mix.shell().info("Successfully Migrated #{file_path}")
+
+        _ ->
+          Mix.shell().error("Unable To Migrate #{file_path}")
+      end
+    end)
+  end
+
+  defp down do
+    [last_migrated_version | versions] = migrated_versions()
+
+    module =
+      last_migrated_version
+      |> migration_path()
+      |> migration_module()
+
+    case apply(module, :down, []) do
+      :ok ->
+        File.cwd!()
+        |> Path.join("lib/mix/tasks/.migrated_versions")
+        |> File.write!(Enum.join(versions, "\n"), [:write])
+
+        Mix.shell().info("Successfully Rolled Back #{last_migrated_version}")
+
+      _ ->
+        Mix.shell().error("Unable To Rollback #{last_migrated_version}")
+    end
+  end
+
+  defp migration_module(path) do
+    {{:module, module, _, _}, _} =
+      File.cwd!()
+      |> Path.join("priv/repo/migrations")
+      |> Path.join(path)
+      |> Code.eval_file()
+
+    module
+  end
+
+  defp migrated_versions do
+    File.cwd!()
+    |> Path.join("lib/mix/tasks/.migrated_versions")
+    |> File.stream!()
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(fn {timestamp, _} -> timestamp end)
+    |> Enum.sort(&(&1 >= &2))
+  end
+
+  defp pending_migrations do
+    File.cwd!()
+    |> Path.join("priv/repo/migrations")
+    |> File.ls!()
+    |> Enum.filter(&(!String.starts_with?(&1, ".")))
+    |> Enum.filter(fn file_path ->
+      {parsed_timestamp, _} =
+        file_path
+        |> timestamp()
+        |> Integer.parse()
+
+      parsed_timestamp not in migrated_versions()
+    end)
+    |> Enum.sort(fn path1, path2 ->
+      timestamp(path1) <= timestamp(path2)
+    end)
+  end
+
+  defp timestamp(path) do
+    path
+    |> String.split("_")
+    |> hd()
+  end
+
+  defp migration_path(version) when not is_binary(version) do
+    version
+    |> to_string()
+    |> migration_path()
+  end
+
+  defp migration_path(version) do
+    File.cwd!()
+    |> Path.join("priv/repo/migrations")
+    |> File.ls!()
+    |> Enum.find(&String.starts_with?(&1, version))
+  end
+end
